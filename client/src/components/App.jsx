10ff5f391ab3bc3c4850d4eb306b44ba741e4381
Dorris Wong <pingki@sbcglobal.net>
2019-08-10 12:26:32 -0700

    Apply conditions for different wind speed

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,14 +2,58 @@ import React from 'react';
 import axios from 'axios';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
+    this.state = {
+      dog: null,
+      header: 0
+    }
+  }
+
+  componentDidMount() {
+    this.getDog();
+  }
+
+  getDog() {
+    axios.get('/api')
+      .then(response => {
+        this.setState({
+          dog: response.data.url,
+          header: response.data.id
+        });
+      }).catch(function (error) {
+        console.log(error);
+    });
   }
 
   render() {
+    let title;
+
+    switch (this.state.header) {
+      case 1:
+        title = 'STAY COOL';
+        break;
+      case 2:
+        title = 'HOODIE KIND OF WEATHER';
+        break;
+      case 3:
+        title = 'LIL CHILLY, WEAR DAT PUFFER';
+        break;
+      case 4:
+        title = 'GONNA BE RAININ\', KEEP YO\' SELF DRY';
+        break;    
+      case 5:
+        title = 'TORNADO, NOTHIN\' YOU CAN DO ABOUT IT';
+        break; 
+      case 6:
+        title = 'ITSA WINDY DAY';
+        break; 
+    }
+
     return (
       <div>
-        <h1>HELLO FROM THE CLIENT</h1>
+        <h1>{title}</h1>
+        <img src = {this.state.dog} />
       </div>
       );
   }
