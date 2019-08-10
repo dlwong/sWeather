ae9be000a655f588d57db026845b2607da741b39
Dorris Wong <pingki@sbcglobal.net>
2019-08-10 00:51:12 -0700

    Connect server to db

--- server/models/index.js
+++ server/models/index.js
@@ -1,9 +1,9 @@
 var db = require('../db');
 
 module.exports.get = function (callback) {
-      var queryStr = 'select * from dogs';
-      
-      db.query(queryStr, function(err, results) {
+      var query = 'select * from dogs';
+
+      db.query(query, function(err, results) {
         callback(err, results);
       });
 };
