d00e4b5160efaaa9b3d4fc541e71f2e1ed222890
Dorris Wong <pingki@sbcglobal.net>
2019-08-10 11:48:18 -0700

    Apply API test logic for controller function

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,23 @@
-var models = require('../models');
+const models = require('../models');
+const api = require('../../helper/weather_api.js');
 
 module.exports.get = function (req, res) {
-      models.get(function(err, results) {
-        if (err) { console.log(err) }
-        res.json(results);
+
+      api.getWind((value) => {
+        // if (err) {
+        //   console.log(err);
+        // }else {
+          console.log('Wind Speed: ',value);
+          models.get(function(err, results) {
+            if (err) {
+              res.sendStatus(404);
+            }else {
+              if (value>5) {
+                res.json(results[0].url);
+              };
+            };
+          });
+       // };
       });
-    }
-  ;
+};
+
