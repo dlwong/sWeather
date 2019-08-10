023c9891cd6d2e3f8a4beedde6e79d58cf761d84
Dorris Wong <pingki@sbcglobal.net>
2019-08-09 18:41:00 -0700

    Set up environment

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, '../client/public')));
+
+app.listen(PORT, () => {
+  console.log(`Running on ${PORT}`);
+});
+
+app.get('/', (req, res) => res.send('Hello World!'))
