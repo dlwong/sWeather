00f17291dcbaff80efdef1bd37da735003b1e7bf
dlwong <starletfordorris@gmail.com>
2020-05-15 15:09:36 -0700

    Add forgot password click functionality

--- client/src/components/Password.jsx
+++ client/src/components/Password.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
@@ -9,22 +9,37 @@ const Wrapper = styled.div`
   transform: translate(-50%, -50%);
 `;
 
-export default function Password() {
-  return (
-        <Wrapper>
-          <div className="l-text-centered">
-            <h2>Reset password</h2>
-            <p>Please enter your email address to request a password reset</p>
-          </div>
-          <form data-test-ref="form">
-            <div className="m-field">
-              <label className="m-field--label">Email Address</label>&nbsp;
-              <input type="email" name="recoverPassword.email" value="" />
-            </div><br/>
-            <button className="m-button bg-azuren" type="submit" tabindex="0">
-              <span className="l-nowrap">Reset Password</span>
-            </button>
-          </form>
-        </Wrapper>
-  )
+class Password extends Component {
+  // constructor(props){
+  //   super(props)
+
+  //   // this.handleForgotPassword = this.handleForgotPassword.bind(this)
+  // }
+
+  handleForgotPassword(evt) {
+    evt.preventDefault();
+    console.log('hello')
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <div className="l-text-centered">
+          <h2>Reset password</h2>
+          <p>Please enter your email address to request a password reset</p>
+        </div>
+        <form data-test-ref="form">
+          <div className="m-field">
+            <label className="m-field--label">Email Address</label>&nbsp;
+            <input type="email" name="recoverPassword.email" value="" />
+          </div><br/>
+          <button className="m-button bg-azuren" onClick={this.handleForgotPassword}>
+            <span className="l-nowrap">Reset Password</span>
+          </button>
+        </form>
+      </Wrapper>
+    )
+  }
 }
+
+export default Password;
