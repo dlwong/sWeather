b8ef0fb7d03e48cb3103bc52a41790bebd30ddb0
dlwong <starletfordorris@gmail.com>
2020-05-15 15:17:34 -0700

    Reconfigure webpack for arrow functions and dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var DIST_DIR = path.join(__dirname, '/client/public');
 var SRC_DIR = path.join(__dirname, '/client/src');
 
 module.exports = {
+  mode: 'development',
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
@@ -16,6 +17,7 @@ module.exports = {
         loader: 'babel-loader',
         query: {
           presets: ['env', 'react'],
+          plugins: ['transform-class-properties']
         }
       }
     ]
