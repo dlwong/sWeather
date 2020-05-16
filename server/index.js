ce7e0925ecd634276f288a40f3058c2485902f88
dlwong <starletfordorris@gmail.com>
2020-05-15 21:55:47 -0700

    Capable to update token in db

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+
 // passport is a modular authentication middlware
 const passport = require('passport'); 
 const mongoose = require('mongoose');
+
 // Bcrypt is a member of a new family of hashing functions that are designed to withstand modern approaches to password cracking. 
 // It is designed to be computationally expensive to compute and it has a configurable work factor that can be used to increase 
 // the expense to help keep pace with the relentless growth in computing power.
@@ -82,10 +84,12 @@ passport.use(new LocalStrategy(
         if (err) {
           return done(err);
         }
+
         // check if user exists
         if (!user) {
           return done(null, false);
         }
+
         // password comparison
         bcrypt.compare(password, user.password, (err, isValid) => {
           if (err) {
@@ -94,6 +98,7 @@ passport.use(new LocalStrategy(
           if (!isValid) {
             return done(null, false)
           }
+
         // passes everything
           return done(null, user)
         })
@@ -117,22 +122,24 @@ app.post('/register', (req, res, next) => {
     password: req.body.password,
     email: req.body.email
   });
-  // salt to fight rainbow table attacks and brute force if someone gains access to the db
+
+  // salt is an extra input to hash. it's to fight rainbow table attacks and brute force if someone gains access to the db
   // salt_work_factor is default 10, but we are being explicit here
   // generate a salt
   bcrypt.genSalt(10, (err, salt) => {
     if (err) {
       return next(err);
     }
+
     // hash the password using our new salt
     bcrypt.hash(newUser.password, salt, (err, hash) => {
       if (err) {
         return next(err);
       }
-      // console.log('before', newUser.password)
+
       // override the cleartext password with the hashed one
       newUser.password = hash; 
-      // console.log('after', newUser.password)
+
       // store the user to the database, then send the response
       newUser.save(function (err) {
         if (err) {
@@ -151,14 +158,27 @@ app.post('/forgotPassword', (req, res) => {
   if (email === ''){
     res.status(400).send('email required')
   }
-  UserDetails.findOneAndUpdate({
-    email: email},{
-    resetPasswordToken: 'hello'
-    }, (err, response) => {
+  UserDetails.findOne({
+    email: email}, (err, user) => {
       if (err){
-        res.send('Error saving:',err)
+        res.status(403).send(err);
       }
-      res.send(response)
+
+      // generate a unique hash
+      const token = crypto.randomBytes(20).toString('hex')
+      user.resetPasswordToken = token;
+
+      // 1 hour token is valid
+      user.resetPasswordExpires = Date.now() + 3600000;
+
+      // update token in db
+      user.save(err => {
+        if (err){
+          res.status(403).send(err);
+          return console.log(err);
+        }
+        res.status(200).send('success');
+      })
     }
   )
 // .then(user => {
